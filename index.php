adb8586ed11c5180ed2b27ea92552a2e77da8434
RenatoBarbosa <rsbarbosa.suporte@gmail.com>
2015-08-03 19:55:25 -0300

    Criando os links do site

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>Atacado</title>
+
+<!-- importação do css -->
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css">
+<link href="css/estilo.css" rel="stylesheet" type="text/css">
+<link href="css/normalize.css" rel="stylesheet" type="text/css">
+
+<!-- importação do Java Script -->
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery-2.1.4.min.js"></script>
+<script src="js/funcoes.js"></script>
+
+</head>
+    <body>
+
+    </body>
+
+</html>
